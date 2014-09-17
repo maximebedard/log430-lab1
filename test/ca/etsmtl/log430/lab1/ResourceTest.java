046f7e6882e3e490d40532ae8957650ad115578e
Maxime Bedard <maxim3.bedard@gmail.com>
2014-09-17 09:16:16 -0400

    Ajout des tests unitaires

---
+++ test/ca/etsmtl/log430/lab1/ResourceTest.java
@@ -0,0 +1,78 @@
+package ca.etsmtl.log430.lab1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ResourceTest {
+
+    @Test
+    public void testAssignProject() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLastName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLastName() throws Exception {
+
+    }
+
+    @Test
+    public void testSetFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void testSetID() throws Exception {
+
+    }
+
+    @Test
+    public void testGetID() throws Exception {
+
+    }
+
+    @Test
+    public void testSetRole() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRole() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPreviouslyAssignedProjectList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPreviouslyAssignedProjectList() throws Exception {
+
+    }
+
+    @Test
+    public void testSetProjectsAssigned() throws Exception {
+
+    }
+
+    @Test
+    public void testGetProjectsAssigned() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWorkLoad() throws Exception {
+
+    }
+}
